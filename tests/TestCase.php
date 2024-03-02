60164b2199e336f3534fff1a54b9c5ce5fe7bf92
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-03-02 15:51:15 +0000

    - Removed spatie/laravel-package-tools.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace IndexZer0\LaravelValidationProvider\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -12,24 +11,10 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'IndexZer0\\LaravelValidationProvider\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [];
     }
 
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-validation-provider_table.php.stub';
-        $migration->up();
-        */
     }
 }
