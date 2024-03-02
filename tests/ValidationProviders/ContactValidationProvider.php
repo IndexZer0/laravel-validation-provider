df2f98db8aefc95fc3af379c932a800ffe20ff2d
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-03-02 04:22:16 +0000

    - ContactValidationProvider class.

---
+++ tests/ValidationProviders/ContactValidationProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\LaravelValidationProvider\Tests\ValidationProviders;
+
+use IndexZer0\LaravelValidationProvider\ValidationProviders\AbstractValidationProvider;
+
+class ContactValidationProvider extends AbstractValidationProvider
+{
+    public function __construct()
+    {
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'min:1'],
+            'home_phone_number' => ['required', 'string', 'min:1'],
+            'mobile_phone_number' => ['required', 'string', 'min:1'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'EMAIL is required',
+            'home_phone_number.required' => 'HOME NUMBER is required',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'phone_number' => 'PHONE NUMBER',
+            'home_phone_number' => 'HOME NUMBER',
+        ];
+    }
+}
