8762d30fb34dce76d407d55b8e918a1f34e1eea9
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-03-04 05:18:59 +0000

    - Added fluent api to interface.

--- src/Contracts/ValidationProvider.php
+++ src/Contracts/ValidationProvider.php
@@ -8,17 +8,37 @@ use Illuminate\Contracts\Validation\Validator;
 
 interface ValidationProvider
 {
+    /*
+     * --------------------------------
+     * Core
+     * --------------------------------
+     */
     public function rules(): array;
-
     public function messages(): array;
-
     public function attributes(): array;
 
-    public function dependentField(string $field): string;
-
+    /*
+     * --------------------------------
+     * Nesting Support
+     * --------------------------------
+     */
+    public function dependentField(string $otherField): string;
     public function prependNestedKey(string $nestedKey): void;
 
+    /*
+     * --------------------------------
+     * Convenience methods
+     * --------------------------------
+     */
     public function createValidator(array $data): Validator;
-
     public function validate(array $data): array;
+
+    /*
+     * --------------------------------
+     * Fluent API
+     * --------------------------------
+     */
+    public function nested(string $nestedKey): ValidationProvider;
+    public function nestedArray(string $nestedKey): ValidationProvider;
+    public function with(string|ValidationProvider $validationProvider): ValidationProvider;
 }
