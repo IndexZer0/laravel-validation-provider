c43acb37867b10d8917412f6fb441c1aaa0007bd
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-03-04 05:24:08 +0000

    - Added CustomValidationProvider.

---
+++ src/ValidationProviders/CustomValidationProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\LaravelValidationProvider\ValidationProviders;
+
+class CustomValidationProvider extends AbstractValidationProvider
+{
+    public function __construct(
+        public readonly array $rules = [],
+        public readonly array $messages = [],
+        public readonly array $attributes = [],
+    ) {
+    }
+
+    public function rules(): array
+    {
+        return $this->rules;
+    }
+
+    public function messages(): array
+    {
+        return $this->messages;
+    }
+
+    public function attributes(): array
+    {
+        return $this->attributes;
+    }
+}
