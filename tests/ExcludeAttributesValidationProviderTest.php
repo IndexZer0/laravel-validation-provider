5405e8096f4db374959bbb985fcf4c5a2056c9e9
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-03-04 15:08:46 +0000

    - Added ExcludeAttributesValidationProviderTest.php

---
+++ tests/ExcludeAttributesValidationProviderTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use IndexZer0\LaravelValidationProvider\Tests\ValidationProviders\AddressValidationProvider;
+use IndexZer0\LaravelValidationProvider\ValidationProviders\ExcludeAttributesValidationProvider;
+use IndexZer0\LaravelValidationProvider\ValidationProviders\NestedValidationProvider;
+
+it('can exclude rules, messages and attributes', function () {
+
+    $validationProvider = new ExcludeAttributesValidationProvider(
+        ['address.post_code', 'address.street'],
+        new NestedValidationProvider(
+            'address',
+            new AddressValidationProvider(),
+        )
+    );
+
+    expect($validationProvider->rules())->toBe([
+        'address.home_phone_number'   => [
+            'required',
+            'string',
+            'min:1',
+        ],
+    ]);
+
+    expect($validationProvider->messages())->toBe([
+        'address.home_phone_number.required' => 'HOME PHONE NUMBER is required',
+    ]);
+
+    expect($validationProvider->attributes())->toBe([
+        'address.home_phone_number' => 'HOME PHONE NUMBER',
+    ]);
+});
