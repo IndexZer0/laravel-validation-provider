d9f645c97d8aa83af579862ee5e224187a8c9eda
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-03-05 00:46:55 +0000

    - Added ValidationProviderExceptionInterface.

---
+++ src/Contracts/ValidationProviderExceptionInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IndexZer0\LaravelValidationProvider\Contracts;
+
+use Throwable;
+
+interface ValidationProviderExceptionInterface extends Throwable
+{
+}
